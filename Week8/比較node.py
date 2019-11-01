5fe09bbcea0d3f914a9889b3843df51b1ee403c6
MorrisLee000 <55569930+MorrisLee000@users.noreply.github.com>
2019-11-01 11:43:24 +0800

    Update 比較node.py

--- Week8/比較node.py
+++ Week8/比較node.py
@@ -2,8 +2,7 @@ class linked_list_node:
   def __init__(self,data):
     self.data = data
     self.next = None   ##用next指定下一個node
-    
-    
+----------------------------------------------------------------  
 class Tree_node:
   def __init__(self,data):
     self.data = data
@@ -11,3 +10,51 @@ class Tree_node:
     self.right = None
     
     
+class linked_list:
+  def __init__(self):
+    self.val = None
+    self.next = None
+    
+  def get(self,index):
+    if self.val == None:
+      return -1
+    if index == 0:
+      return self.val
+    i = 1
+    while self.next != None:
+      if i == index:
+        return self.next.val
+      self.next = self.next.next
+      i += 1
+    return -1
+-------------------------------------------------
+class Tree:
+  def __init__(self):
+    self.val = None
+    self.next = None
+  
+  def getleft(self.index):
+    if self.val == None:
+      return -1
+    if index == 0:
+      return self.val
+    i = 1
+    while self.left != None:
+      if i == index:
+        return self.left.val
+      self.left = self.left.left
+      i += 1
+    return -1
+  
+  def getright(self.index):
+    if self.val == None:
+      return -1
+    if index == 0:
+      return self.val
+    i = 1
+    while self.right != None:
+      if i == index:
+        return self.right.val
+      self.left = self.right.right
+      i += 1
+    return -1

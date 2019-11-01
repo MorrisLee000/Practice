fce7de4bb753c040e94f80173cef1f77a9be9bae
MorrisLee000 <55569930+MorrisLee000@users.noreply.github.com>
2019-11-01 11:33:38 +0800

    Update tree.py

--- Week8/tree.py
+++ Week8/tree.py
@@ -1,9 +1,13 @@
-class linked_list:
+class linked_list_node:
   def __init__(self,data):
     self.data = data
-    self.next = None
-class Tree:
+    self.next = None   ##用next指定下一個node
+    
+    
+class Tree_node:
   def __init__(self,data):
     self.data = data
-    self.left = None
+    self.left = None   ##把next轉換成left和right來指定下一個node
     self.right = None
+    
+    

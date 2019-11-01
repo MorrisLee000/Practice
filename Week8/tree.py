5e5457584a164e82755b67078dc2246944d2069c
MorrisLee000 <55569930+MorrisLee000@users.noreply.github.com>
2019-11-01 11:30:31 +0800

    Update tree.py

--- Week8/tree.py
+++ Week8/tree.py
@@ -1,1 +1,9 @@
-
+class linked_list:
+  def __init__(self,data):
+    self.data = data
+    self.next = None
+class Tree:
+  def __init__(self,data):
+    self.data = data
+    self.left = None
+    self.right = None

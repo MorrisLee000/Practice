aab1adbd9cd425e923e72d40fd98f8f6220fd8a0
MorrisLee000 <55569930+MorrisLee000@users.noreply.github.com>
2019-11-01 12:03:53 +0800

    Update 比較node.py

--- Week8/比較node.py
+++ Week8/比較node.py
@@ -8,8 +8,7 @@ class Tree_node:
     self.data = data
     self.left = None   ##把next轉換成left和right來指定下一個node
     self.right = None
-    
-    
+----------------------------------------------------------------    
 class linked_list:
   def __init__(self):
     self.val = None
@@ -27,13 +26,15 @@ class linked_list:
       self.next = self.next.next
       i += 1
     return -1
--------------------------------------------------
+  
+  def addAtTail(self,val):
+-----------------------------------------------------------------
 class Tree:
   def __init__(self):
     self.val = None
     self.next = None
   
-  def getleft(self.index):
+  def get(self.index):
     if self.val == None:
       return -1
     if index == 0:
@@ -45,20 +46,14 @@ class Tree:
       self.left = self.left.left
       i += 1
     return -1
-  
-  def getright(self.index):
-    if self.val == None:
-      return -1
-    if index == 0:
-      return self.val
-    i = 1
     while self.right != None:
       if i == index:
         return self.right.val
-      self.left = self.right.right
+      self.right = self.right.right
       i += 1
     return -1
-
+  
+  def addAtTail(self,val):
   
   ##參考老師的linkedlist
   ##參考網址https://www.tutorialspoint.com/python_data_structure/python_linked_lists.htm

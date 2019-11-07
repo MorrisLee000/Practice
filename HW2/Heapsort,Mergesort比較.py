28db9b49ea27f5ddbb888ed6d193ffaa9d5f0ab7
MorrisLee000 <55569930+MorrisLee000@users.noreply.github.com>
2019-11-08 00:52:36 +0800

    Create Heapsort,Mergesort比較.py

---
+++ HW2/Heapsort,Mergesort比較.py
@@ -0,0 +1,8 @@
+##Heap sort
+##1.heapsort 是先排成樹狀每一個點都有元素，而且每個元素只能接兩個元素。
+##2.heapsort 不需要額外使用list。
+##3.heapsort 每次進行一次maxheap結束後把最大的元素和最後一個元素對調位置然後提出，避免它又重複進入maxheap。
+##Merge sort
+##1.mergesort 是分成兩堆兩堆，直到分到每個list都只有個元素。
+##2.mergesort 一開始會需要額外兩個list來存放兩堆資料。
+##3.mergesort 是用一堆一堆內的元素來比較，排列完後再往上一層和另一堆的元素比較。

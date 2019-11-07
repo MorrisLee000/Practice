eed46c9e1092f45a17dc694bb590f51bb3a74868
MorrisLee000 <55569930+MorrisLee000@users.noreply.github.com>
2019-11-08 00:46:33 +0800

    Update heap_sort_06170243.py

--- HW2/heap_sort_06170243.py
+++ HW2/heap_sort_06170243.py
@@ -1,1 +1,30 @@
+class Solution(object):
+    def heap_sort(self,nums):
+        self.nums = nums
+        n = len(nums)
 
+        for i in range(n, -1, -1):
+            Solution().heapify(nums, n, i)
+
+        for i in range(n-1, 0, -1):    
+            nums[i], nums[0] = nums[0], nums[i]
+            Solution().heapify(nums, i, 0)
+
+        return nums
+
+    def heapify(self,nums, n, i):
+        large = i
+        l = 2 * i + 1
+        r = 2 * i + 2
+        if l < n and nums[i] < nums[l]:
+            large = l
+        if r < n and nums[l] < nums[r]:
+            large = r
+
+        if large != i:
+            nums[i], nums[large] = nums[large], nums[i]
+
+            Solution().heapify(nums, n, large)
+
+            
+參考網址 https://www.geeksforgeeks.org/python-program-for-heap-sort/

9a4d073d5e89d7467a41381dcc0e1a9a50de2cbc
MorrisLee000 <55569930+MorrisLee000@users.noreply.github.com>
2019-11-08 11:30:06 +0800

    Update binary search tree.py

--- Week9/binary search tree.py
+++ Week9/binary search tree.py
@@ -1,1 +1,47 @@
+class Node:
+  def __init__(self,data):
+    self.data = data
+    self.left = None
+    self.right = None
 
+  def insert(self, data):
+    if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.data = data
+  
+  
+  if 
+  
+    
+  def find(self, data):
+    if self.data == data:
+      return True
+    elif data < self.data and self.left:
+      return self.left.find(data)
+    elif data > self.data and self.right:
+      return self.right.find(data)
+    return False
+
+class Binary_Search_Tree:
+	def __init__(self, data):
+    self.data = data
+    self.left = None
+    self.right = None
+    
+  def insert(self, data):
+		if self.root:
+			return self.root.insert(d)
+		else:
+			self.root = Node(d)
+			return True
+  

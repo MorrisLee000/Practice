44e9b7e78da74785f16b2d6e71422289f3c8570b
MorrisLee000 <55569930+MorrisLee000@users.noreply.github.com>
2019-11-12 10:53:38 +0800

    Update binary_search_tree_06170243.py

--- HW3/binary_search_tree_06170243.py
+++ HW3/binary_search_tree_06170243.py
@@ -1,1 +1,18 @@
+class Node:
+    
+class Binary_Search_Tree:
+    def __init__(self, nums):
+        self.nums = nums
+        self.left = left
+        self.right = right
+    
+##插入
+    def insert(self, nums)
+  
+##刪除
 
+
+##尋找
+
+
+##取代

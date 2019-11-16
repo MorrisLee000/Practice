c1767dd4792c94cae981aefd292bf164042fb195
MorrisLee000 <55569930+MorrisLee000@users.noreply.github.com>
2019-11-16 15:00:50 +0800

    Update binary_search_tree_06170243.py

--- HW3/binary_search_tree_06170243.py
+++ HW3/binary_search_tree_06170243.py
@@ -1,14 +1,26 @@
 class Node:
-    
-class Binary_Search_Tree:
     def __init__(self, nums):
         self.nums = nums
-        self.left = left
-        self.right = right
+        self.left = None
+        self.right = None
+
+ class Binary_Search_Tree:
+    def __init__(self, root):
+        self.root = None
     
 ##插入
     def insert(self, nums)
-  
+        if self.root == None:
+            self.root = Node(nums)
+        else:
+            self._insert(nums, self.root)
+    
+    def _insert(self, nums, cur_node):
+        if nums > cur_node.nums:
+            if cur_node.left == None:
+                cur_node.left = Node(nums)
+            else:
+                self._insert
 ##刪除
 
 

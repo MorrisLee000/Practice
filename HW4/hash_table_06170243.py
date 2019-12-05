83119605cef753f861b1269bd62d75d6cb5bf920
MorrisLee000 <55569930+MorrisLee000@users.noreply.github.com>
2019-12-06 00:54:40 +0800

    Update hash_table_06170243.py

--- HW4/hash_table_06170243.py
+++ HW4/hash_table_06170243.py
@@ -1,6 +1,62 @@
 from Crypto.Hash import MD5
 
-
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+        
+class MyHashSet:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.data = [None] * self.capacity
+    
+    def add(self, key):
+        h = MD5.new()                   
+        h.update(key.encode("UTF-8"))   
+        x = int(h.hexdigest(), 16)      
+        k = x % self.capacity           
+        target = self.data[k]           
+        while target:                   
+            if target.val == x:         
+                return                  
+            else:
+                target = target.next    
+        new_listnode = ListNode(x)      
+        new_listnode.next = self.data[k]
+        self.data[k] = new_listnode     
+    
+    def remove(self, key):
+        h = MD5.new()
+        h.update(key.encode("UTF-8"))
+        x = int(h.hexdigest(),16)
+        k = x % self.capacity
+        target = self.data[k]        
+        while target:
+            if target.val == x:
+                self.data[k] = target.next
+                return
+            else:
+                previous = None
+            while target:
+                if target.val == x:
+                    previous.next = target.next
+                    return
+                else:
+                    previous = target
+                    target = target.next
+              
+    def contains(self, key):
+        h = MD5.new()
+        h.update(key.encode("UTF-8"))
+        x = int(h.hexdigest(), 16)
+        k = x % self.capacity
+        target = self.data[k]
+        while target:
+            if target.val == x:      
+                return True          
+            else:                    
+                target = target.next
+        return False
 
 
 #參考網址1 https://www.youtube.com/watch?v=7C5f2ttq79Y&feature=youtu.be

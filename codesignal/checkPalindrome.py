85970691a5a17f5ced3a3dba52b598e1ae18534b
MorrisLee000 <55569930+MorrisLee000@users.noreply.github.com>
2020-01-03 21:10:02 +0800

    Create checkPalindrome.py

---
+++ codesignal/checkPalindrome.py
@@ -0,0 +1,16 @@
+def checkPalindrome(inputString):
+    i = inputString
+    a = len(i)
+    b = 0
+    count = 1
+    while b <= (a/2):
+        if i[b] == i[a-b-1]:
+            count = 1
+            b += 1
+        else:
+            count = 0
+            break
+    if count == 1:
+        return True
+    else:
+        return False
